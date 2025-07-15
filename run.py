92bcffe95402406b10a9e498569ec2101efe0c05
Dakar2024 <mantsouakaarthur10@gmail.com>
2025-07-15 13:24:24 +0000

    ajout d'une boucle while au niveau console de run.py

--- run.py
+++ run.py
@@ -22,16 +22,17 @@ def process(email_text: str) -> str:
     return "normal" if prediction == 0 else "spam"
 
 if __name__ == "__main__":
-    print("=== Détection de SPAM dans les SMS ===\n")
-    print("Exemples de messages à tester :")
-    print("  - 'Félicitations ! Vous avez gagné un iPhone. Cliquez ici !'")
-    print("  - 'Salut, tu viens ce soir ?'")
-    print("  - 'URGENT: Votre compte sera suspendu. Mettez à jour vos infos.'\n")
+    while True :
+        print("=== Détection de SPAM dans les SMS ===\n")
+        print("Exemples de messages à tester :")
+        print("  - 'Félicitations ! Vous avez gagné un iPhone. Cliquez ici !'")
+        print("  - 'Salut, tu viens ce soir ?'")
+        print("  - 'URGENT: Votre compte sera suspendu. Mettez à jour vos infos.'\n")
 
-    user_input = input("Entrez un message à analyser :\n> ")
+        user_input = input("Entrez un message à analyser :\n> ")
 
-    if not user_input.strip():
-        print("\nAucun message entré. Veuillez réessayer.")
-    else:
-        result = process(user_input)
-        print(f"\nRésultat : Ce message est probablement : {result.upper()}")
+        if not user_input.strip():
+            print("\nAucun message entré. Veuillez réessayer.")
+        else:
+            result = process(user_input)
+            print(f"\nRésultat : Ce message est probablement : {result.upper()}")
